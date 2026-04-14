05ad55e10b75fa8ed42dbf504cede21487c5dd29
Joaco-Lumbrera <jlumbrera@frba.utn.edu.ar>
2026-04-14 00:22:38 +0000

    ahi hice la conexion con scheduler

--- io/src/main.c
+++ io/src/main.c
@@ -1,6 +1,36 @@
 #include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
+
+
 
 int main(int argc, char* argv[]) {
-    saludar("io");
+
+    //Creo el logger
+    t_log* loggerIO=log_create("io.log", "main.c", true, LOG_LEVEL_INFO);
+    //Creo el config
+    t_config* configIO = config_create(argv[1]);
+    //Defino las variables para conectarme al scheduler
+    int socketConScheduler;
+    char* ip=config_get_string_value(configIO,"IP_KERNEL");
+    char* puerto= config_get_string_value(configIO,"PUERTO_KERNEL");
+
+
+    //Creo conexion con Scheduler
+    socketConScheduler = iniciar_conexion(ip,puerto);
+    if(socketConScheduler == EXIT_FAILURE){
+        log_info(loggerIO, "no se pudo conectar a Kernel Scheduler");
+        //ver si hay que abortar
+    }
+
+    log_info(loggerIO, "conexion establecida con Kernel Scheduler");
+    
+    handshake_cliente(socketConScheduler,loggerIO);
+
+
+
     return 0;
 }
+
+
+
