a45818c0dda05ddef32b5db139795ec7d65f04fc
mviotti1 <mviotti@frba.utn.edu.ar>
2026-05-22 13:28:39 +0000

    CPU: mejoro el manejo de interrupciones

--- cpu/src/interrupciones.c
+++ cpu/src/interrupciones.c
@@ -4,23 +4,27 @@
 
 uint32_t motivo_interrupcion = -1;
 
-// Chequea (sin bloquear) si llegó un paquete de interrupción del scheduler.
-// Se llama entre instrucciones, por lo que el main thread es el único lector del socket.
+// select() es un recv() con timeout 0, es decir, no bloqueante.
 bool hay_interrupcion(uint32_t pidActual, int socketConexionScheduler) {
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(socketConexionScheduler, &readfds);
-    struct timeval tv = {0, 0};
+    fd_set readfds; //Lista e sockets donde escuchar
+    FD_ZERO(&readfds); //Limpio la lista
+    FD_SET(socketConexionScheduler, &readfds); //Agrego el socket de scheduler pq quiero escuchar ahi
+    struct timeval tv = {0, 0}; //Timeout 0, es decir, no bloqueante
 
-    if (select(socketConexionScheduler + 1, &readfds, NULL, NULL, &tv) <= 0)
+    //select(numero mas alto de descriptor, lista, es de escritura?, es de error?, timeout)
+    if (select(socketConexionScheduler + 1, &readfds, NULL, NULL, &tv) <= 0) {
+        //Si devuelve > 0, hay datos para leer, si devuelve 0, no hay datos (timeout), y si devuelve < 0, hubo un error.
         return false;
+    }
 
+    //Si sigue es que hay datos para leer
     t_paquete *paquete = recibir_paquete(socketConexionScheduler);
-    if (paquete->codigo_operacion == FINALIZAR_POR_QUANTUM) {
+    op_code interrupcionRecibida = paquete->codigo_operacion;
+    if ((interrupcionRecibida == FINALIZAR_POR_QUANTUM) || (interrupcionRecibida == PROCESO_BLOQUEADO)) {
         uint32_t pidInterrumpido = buffer_read_uint32(paquete->buffer);
         eliminar_paquete(paquete);
         if (pidInterrumpido == pidActual) {
-            motivo_interrupcion = FINALIZAR_POR_QUANTUM;
+            motivo_interrupcion = interrupcionRecibida;
             log_info(loggerCpu, "## Interrupción recibida");
             return true;
         }
