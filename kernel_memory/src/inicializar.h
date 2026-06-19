985b335df76cd3d0b2e421c20a3fb81edf69cd43
JuanGalzerano <jgalzerano@frba.utn.edu.ar>
2026-06-19 00:21:21 +0000

    socket q no cerraba, lock en buscar proceso, ahora esta la alternativa de sin lockear, elimine lo de datos_... que no servia para nada

--- kernel_memory/src/inicializar.h
+++ kernel_memory/src/inicializar.h
@@ -7,6 +7,7 @@ void              inicializar_log_y_config(char* path);
 int               inicializar_proceso(uint32_t pid, char* path);
 void              finalizar_proceso(uint32_t pid);
 t_proceso_memory* buscar_proceso(uint32_t pid);
+t_proceso_memory* buscar_proceso_sin_lock(uint32_t pid);
 char*             leer_instruccion(t_proceso_memory* proceso, uint32_t pc);
 
 #endif
